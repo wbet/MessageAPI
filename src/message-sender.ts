fcbe201d62cc10b0e76ab67721dceaf289290e9b
Iulian Preda <iulian.octavian.preda@gmail.com>
2021-03-07 22:30:43 +0100

    Fixed type definitions

--- src/message-sender.ts
+++ src/message-sender.ts
@@ -1,47 +1,46 @@
 import { MessageLocation } from './enums/message-location';
-import { Message, MessageOptions, MessagePath, MessagePathFilter, SendMessageParams } from './interfaces/message.interface';
+import { Message, MessageOptions, MessagePathFilter, SendMessageParams } from './interfaces/message.interface';
 import { callbackToPromise, checkChrome, getCurrentTab } from './lib/helpers';
 
 /** Sends message to the background script */
-export function sendMessageToBackground<T = any>(message: any, source: MessageLocation, messageOptions?: MessageOptions, filters?: MessagePathFilter) {
-    return sendMessageTo<T>(message, { source, destination: MessageLocation.Background, ...filters }, messageOptions);
+export function sendMessageToBackground<TData = any, TResponseData = any>(data: TData, source: MessageLocation, messageOptions?: MessageOptions, filters?: MessagePathFilter) {
+    return sendMessageTo<TData, TResponseData>({ data, source, destination: MessageLocation.Background, ...filters }, messageOptions);
 }
 
 /** Sends message to the options page */
-export function sendMessageToOptions<T = any>(message: any, source: MessageLocation, messageOptions?: MessageOptions, filters?: MessagePathFilter) {
-    return sendMessageTo<T>(message, { source, destination: MessageLocation.Options, ...filters }, messageOptions);
+export function sendMessageToOptions<TData = any, TResponseData = any>(data: TData, source: MessageLocation, messageOptions?: MessageOptions, filters?: MessagePathFilter) {
+    return sendMessageTo<TData, TResponseData>({ data, source, destination: MessageLocation.Options, ...filters }, messageOptions);
 }
 
 /** Sends message to the popup page */
-export function sendMessageToPopup<T = any>(message: any, source: MessageLocation, messageOptions?: MessageOptions, filters?: MessagePathFilter) {
-    return sendMessageTo<T>(message, { source, destination: MessageLocation.Popup, ...filters }, messageOptions);
+export function sendMessageToPopup<TData = any, TResponseData = any>(data: TData, source: MessageLocation, messageOptions?: MessageOptions, filters?: MessagePathFilter) {
+    return sendMessageTo<TData, TResponseData>({ data, source, destination: MessageLocation.Popup, ...filters }, messageOptions);
 }
 
 /** Sends message to a specific content script */
-export function sendMessageToContentScript<T = any>(tabId: number, message: any, source: MessageLocation, messageOptions?: MessageOptions, filters?: MessagePathFilter) {
-    return sendMessageTo<T>(message, { source, destination: MessageLocation.Content, ...filters }, messageOptions, tabId);
+export function sendMessageToContentScript<TData = any, TResponseData = any>(tabId: number, data: TData, source: MessageLocation, messageOptions?: MessageOptions, filters?: MessagePathFilter) {
+    return sendMessageTo<TData, TResponseData>({ data, source, destination: MessageLocation.Content, ...filters }, messageOptions, tabId);
 }
 
 /** Sends message to a the currently active webpage/content script */
-export async function sendMessageToCurrentWebPage<T = any>(message: any, source: MessageLocation, messageOptions?: MessageOptions, filters?: MessagePathFilter) {
-    return sendMessageTo<T>(message, { source, destination: MessageLocation.Content, ...filters }, messageOptions, (await getCurrentTab()).id);
+export function sendMessageToCurrentWebPage<TData = any, TResponseData = any>(data: TData, source: MessageLocation, messageOptions?: MessageOptions, filters?: MessagePathFilter) {
+    return getCurrentTab().then((x) => sendMessageTo<TData, TResponseData>({ data, source, destination: MessageLocation.Content, ...filters }, messageOptions, x.id));
 }
 
 /** Sends message to a custom location */
-export function sendMessageTo<T = any>(message: Message, path: MessagePath, messageOptions?: MessageOptions, tabId?: number) {
-    let messageToSend = Object.assign(message, path);
+export function sendMessageTo<TData = any, TResponseData = any>(message: Message<TData>, messageOptions?: MessageOptions, tabId?: number) {
     if (messageOptions?.extensionId) {
-        return sendMessage<T>(messageOptions.extensionId, messageToSend, messageOptions?.options);
+        return sendMessage<TResponseData>(messageOptions.extensionId, message, messageOptions?.options);
     }
 
     if (tabId) {
-        return sendMessageFromTab<T>(tabId, messageToSend, messageOptions?.options);
+        return sendMessageFromTab<TResponseData>(tabId, message, messageOptions?.options);
     }
 
-    return sendMessage<T>(messageToSend, messageOptions?.options);
+    return sendMessage<TResponseData>(message, messageOptions?.options);
 }
 
-function sendMessage<T = any>(...args: SendMessageParams): Promise<T> {
+function sendMessage<TResponseData = any>(...args: SendMessageParams): Promise<TResponseData> {
     const runtimeType = globalThis.browser ?? globalThis.chrome;
     if (checkChrome()) {
         return callbackToPromise(runtimeType.runtime.sendMessage, args);
@@ -49,7 +48,7 @@ function sendMessage<T = any>(...args: SendMessageParams): Promise<T> {
     return runtimeType.runtime.sendMessage(args);
 }
 
-function sendMessageFromTab<T = any>(tabId: number, ...args: SendMessageParams): Promise<T> {
+function sendMessageFromTab<TResponseData = any>(tabId: number, ...args: SendMessageParams): Promise<TResponseData> {
     const runtimeType = globalThis.browser ?? globalThis.chrome;
     if (checkChrome()) {
         return callbackToPromise(runtimeType.tabs.sendMessage, [tabId, ...args]);
